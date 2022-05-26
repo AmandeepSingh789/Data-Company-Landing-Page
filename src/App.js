eff9fccafe2b775216b5bb551ce8132fe3052aed
Amandeep <singh.amandeep5665@gmail.com>
2022-05-25 20:27:26 -0700

    created plans component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Analytics from "./components/Analytics";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
+import Plans from "./components/Plans";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Analytics />
       <Newsletter />
+      <Plans />
     </div>
   );
 }
---
+++ src/components/Plans.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import single from '../assets/single.png'
+import double from '../assets/double.png'
+import triple from '../assets/triple.png'
+
+
+const Plans = () => {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+
+            <div className='w-full border '>
+                <img src={single} alt="Single User" className='w-20 mx-auto mt-[-3rem] bg-white' />
+                <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+                <p>$99</p>
+                <div>
+                    <p>500 GB Storage</p>
+                    <p>1 User </p>
+                    <p>Upload speeds upto 100 mb/s</p>
+                </div>
+                <button>Start Trial</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Plans
