ee4612d9268bc4790851ea8ca7825bc5c3d07fcb
Amandeep <singh.amandeep5665@gmail.com>
2022-05-26 20:11:52 -0700

    completed plans page

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -14,17 +14,42 @@ const Plans = () => {
          </div>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
 
-            <div className='w-full border '>
+            <div className='w-full shadow-2xl shadow-[#5FD21F] flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500 mb-16'>
                 <img src={single} alt="Single User" className='w-20 mx-auto mt-[-3rem] bg-white' />
-                <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-                <p>$99</p>
-                <div>
-                    <p>500 GB Storage</p>
-                    <p>1 User </p>
-                    <p>Upload speeds upto 100 mb/s</p>
+                <h2 className='text-4xl font-bold text-center py-8'>Single User</h2>
+                <p className='text-center text-5xl font-bold'>$99</p>
+                <div className='text-center font-lg text-xl'>
+                    <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                    <p className='py-2 border-b mx-8'>1 User </p>
+                    <p className='py-2 border-b mx-8'>Upload speeds upto 100 mb/s</p>
                 </div>
-                <button>Start Trial</button>
+                <button className='md:text-xl sm:text-xl text-lg bg-[#5FD21F] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-4 py-3 hover:text-white'>Start Trial</button>
             </div>
+            <div className='w-full shadow-2xl shadow-[#5FD21F] flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500 mb-16'>
+                <img src={double} alt="Single User" className='w-20 mx-auto mt-[-3rem] bg-white' />
+                <h2 className='text-4xl font-bold text-center py-8'>Double User</h2>
+                <p className='text-center text-5xl font-bold'>$149</p>
+                <div className='text-center font-lg text-xl'>
+                    <p className='py-2 border-b mx-8 mt-8'>1000 GB Storage</p>
+                    <p className='py-2 border-b mx-8'>2 Users </p>
+                    <p className='py-2 border-b mx-8'>Upload speeds upto 500 mb/s</p>
+                </div>
+                <button className='md:text-xl sm:text-xl text-lg bg-[#5FD21F] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-4 py-3 hover:text-white'>Start Trial</button>
+            </div>
+            <div className='w-full shadow-2xl shadow-[#5FD21F] flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-500 mb-16'>
+                <img src={triple} alt="Single User" className='w-20 mx-auto mt-[-3rem] bg-white' />
+                <h2 className='text-4xl font-bold text-center py-8'>Triple User</h2>
+                <p className='text-center text-5xl font-bold'>$199</p>
+                <div className='text-center font-lg text-xl'>
+                    <p className='py-2 border-b mx-8 mt-8'>2000 GB Storage</p>
+                    <p className='py-2 border-b mx-8'>3 Users </p>
+                    <p className='py-2 border-b mx-8'>Upload speeds upto 1000 mb/s</p>
+                </div>
+                <button className='md:text-xl sm:text-xl text-lg bg-[#5FD21F] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-4 py-3 hover:text-white'>Start Trial</button>
+            </div>
+            
+
+          
         </div>
     </div>
   )
