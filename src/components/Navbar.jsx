1b87570e635bf9f416d9a8ff0ac4b4e8731d6658
Amandeep <singh.amandeep5665@gmail.com>
2022-05-26 20:44:31 -0700

    finished hovering feature on things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 const Navbar = () => {
 
-    const[nav,setNav] = useState(true)
+    const[nav,setNav] = useState(false)
 
     const handleNav = () =>{
 
@@ -23,20 +23,20 @@ const Navbar = () => {
         </ul>
 
         <div onClick={handleNav} className="block md:hidden">
-            {!nav ? <BsMenuButtonFill size={30} className="hover:text-[#75FF26] hover:cursor-pointer" /> : <BsMenuButtonWideFill size={30}  className="hover:text-[#75FF26] hover:cursor-pointer"/ >}
+            {nav ? <BsMenuButtonFill size={30} className="hover:text-[#75FF26] hover:cursor-pointer" /> : <BsMenuButtonWideFill size={30}  className="hover:text-[#75FF26] hover:cursor-pointer"/ >}
             
         </div>
 
-        {<div className={ !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-[#75FF26] bg-[#0C0C0C] ease-in-out duration-700' : 'fixed left-[-100%]'}>
+        {<div className={ nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-[#75FF26] bg-[#0C0C0C] ease-in-out duration-700' : 'fixed left-[-100%]'}>
             
         <h1 className='w-full text-5xl p-4 font-bolds text-[#75FF26] font-Inconsolata'>REACT.</h1>
             
             <ul className='uppercase p-4 font-Inconsolata'>
-            <li className='p-4 border-b border-[#75FF26] '>Home</li>
-            <li className='p-4  border-b border-[#75FF26]'>Company</li>
-            <li className='p-4  border-b border-[#75FF26]'>Resources</li>
-            <li className='p-4  border-b border-[#75FF26]'>About</li>
-            <li className='p-4 '>Contact</li>
+            <li className='p-4 border-b border-[#75FF26] hover:cursor-pointer  hover:text-[#75FF26]'>Home</li>
+            <li className='p-4  border-b border-[#75FF26] hover:cursor-pointer  hover:text-[#75FF26]'>Company</li>
+            <li className='p-4  border-b border-[#75FF26] hover:cursor-pointer  hover:text-[#75FF26]'>Resources</li>
+            <li className='p-4  border-b border-[#75FF26] hover:cursor-pointer  hover:text-[#75FF26]'>About</li>
+            <li className='p-4 hover:cursor-pointer  hover:text-[#75FF26] '>Contact</li>
             </ul>
         </div>}
     </div>
